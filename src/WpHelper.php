218ed8fd71c2d98b2f8ff2d64993190c617404cc
Björn Hasse <hasse@onvard.de>
2018-06-19 12:30:23 +0200

    WpHelper: Add fileUploadMaxSize function

--- src/WpHelper.php
+++ src/WpHelper.php
@@ -67,4 +67,46 @@ class WpHelper
         // @see https://github.com/WordPress/WordPress/blob/master/wp-includes/shortcodes.php#L596
         return strip_shortcodes($excerpt);
     }
+
+    /**
+     * Returns a file size limit in bytes based on the PHP upload_max_filesize and post_max_size
+     * @see https://stackoverflow.com/questions/13076480/php-get-actual-maximum-upload-size
+     * @return int
+     */
+    public static function fileUploadMaxSize() {
+        static $max_size = -1;
+
+        if ($max_size < 0) {
+            // Start with post_max_size.
+            $post_max_size = static::parseSize(ini_get('post_max_size'));
+            if ($post_max_size > 0) {
+                $max_size = $post_max_size;
+            }
+
+            // If upload_max_size is less, then reduce. Except if upload_max_size is
+            // zero, which indicates no limit.
+            $upload_max = static::parseSize(ini_get('upload_max_filesize'));
+            if ($upload_max > 0 && $upload_max < $max_size) {
+                $max_size = $upload_max;
+            }
+        }
+        return $max_size;
+    }
+
+    /**
+     * Parses the ini filesize string and returns the number of bytes
+     * @see https://stackoverflow.com/questions/13076480/php-get-actual-maximum-upload-size
+     *
+     * @param $size
+     * @return int
+     */
+    public static function parseSize($size) {
+        $unit = preg_replace('/[^bkmgtpezy]/i', '', $size); // Remove the non-unit characters from the size.
+        $size = preg_replace('/[^0-9\.]/', '', $size); // Remove the non-numeric characters from the size.
+        if ($unit) {
+            // Find the position of the unit in the ordered string which is the power of magnitude to multiply a kilobyte by.
+            return (int) round($size * pow(1024, stripos('bkmgtpezy', $unit[0])));
+        }
+        return (int) round($size);
+    }
 }
